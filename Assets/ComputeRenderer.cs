27c4d345abbf5f166991795d789ef41db949e602
cookie1170 <171882521+cookie1170@users.noreply.github.com>
2025-06-27 16:27:40 +1000

    stuff renders to screen :comfy:

--- Assets/ComputeRenderer.cs
+++ Assets/ComputeRenderer.cs
@@ -1,6 +1,43 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class ComputeRenderer : MonoBehaviour
 {
-	
+	private static readonly int Result = Shader.PropertyToID("Result");
+	[SerializeField] private ComputeShader shader;
+	private Camera _cam;
+	[SerializeField] private RenderTexture _rt;
+	private int _kernelIndex;
+
+	private void Awake()
+	{
+		_cam = GetComponent<Camera>();
+		_cam.targetTexture = _rt;
+		RenderPipelineManager.endContextRendering += OnEndContextRendering;
+		RenderPipelineManager.beginContextRendering += OnBeginContextRendering;
+		_kernelIndex = shader.FindKernel("CSMain");
+	}
+
+	private void OnBeginContextRendering(ScriptableRenderContext ctx, List<Camera> cameras)
+	{
+		_rt.Release();
+		_rt = new(Screen.width, Screen.height, 24)
+		{
+			enableRandomWrite = true
+		};
+	}
+
+	private void OnEndContextRendering(ScriptableRenderContext ctx, List<Camera> cameras)
+	{
+		shader.SetTexture(_kernelIndex, Result, _rt);
+		shader.Dispatch(_kernelIndex, _rt.width / 8, _rt.height / 8, 1);
+		Graphics.Blit(_rt, dest: (RenderTexture)null);
+	}
+
+	private void OnDestroy()
+	{
+		RenderPipelineManager.endContextRendering -= OnEndContextRendering;
+		RenderPipelineManager.beginContextRendering -= OnBeginContextRendering;
+	}
 }
