7e2da8a2be27ffc2fa29bad7d7ab3c6b66921012
cookie1170 <171882521+cookie1170@users.noreply.github.com>
2025-06-28 18:12:34 +1000

    circle(s) :pepehype:

--- Assets/ComputeRenderer.cs
+++ Assets/ComputeRenderer.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
 public class ComputeRenderer : MonoBehaviour
 {
-	private static readonly int Result = Shader.PropertyToID("Result");
+	private static readonly int ScreenTextureID = Shader.PropertyToID("screenTexture");
+	private static readonly int ResolutionID = Shader.PropertyToID("resolution");
+	private static readonly int CircleBufferID = Shader.PropertyToID("circleBuffer");
+	[SerializeField] private List<Circle> circles = new();
 	[SerializeField] private ComputeShader shader;
+	private RenderTexture _rt;
 	private Camera _cam;
-	[SerializeField] private RenderTexture _rt;
 	private int _kernelIndex;
 
-	private void Awake()
+	[Serializable]
+	struct Circle
+	{
+		public Vector2 position;
+		public float radius;
+	}
+
+private void Awake()
 	{
 		_cam = GetComponent<Camera>();
+		_rt = new(Screen.width, Screen.height, 24)
+		{
+			enableRandomWrite = true
+		};
 		_cam.targetTexture = _rt;
 		RenderPipelineManager.endContextRendering += OnEndContextRendering;
 		RenderPipelineManager.beginContextRendering += OnBeginContextRendering;
@@ -21,7 +36,7 @@ public class ComputeRenderer : MonoBehaviour
 
 	private void OnBeginContextRendering(ScriptableRenderContext ctx, List<Camera> cameras)
 	{
-		_rt.Release();
+		_rt?.Release();
 		_rt = new(Screen.width, Screen.height, 24)
 		{
 			enableRandomWrite = true
@@ -30,9 +45,14 @@ public class ComputeRenderer : MonoBehaviour
 
 	private void OnEndContextRendering(ScriptableRenderContext ctx, List<Camera> cameras)
 	{
-		shader.SetTexture(_kernelIndex, Result, _rt);
+		ComputeBuffer circleBuffer = new(circles.Count, sizeof(float) * 3);
+		circleBuffer.SetData(circles);
+		shader.SetVector(ResolutionID, new Vector2(Screen.width, Screen.height));
+		shader.SetTexture(_kernelIndex, ScreenTextureID, _rt);
+		shader.SetBuffer(_kernelIndex, CircleBufferID, circleBuffer);
 		shader.Dispatch(_kernelIndex, _rt.width / 8, _rt.height / 8, 1);
 		Graphics.Blit(_rt, dest: (RenderTexture)null);
+		circleBuffer.Release();
 	}
 
 	private void OnDestroy()
